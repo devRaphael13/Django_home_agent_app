fed45a66799a77466732ddc46887a27f0005b1c7
Raphael Ezeigwe <iraphael1308@gmail.com>
2023-01-30 14:30:05 +0100

    deployment

--- Agent/settings.py
+++ Agent/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-import django_heroku
 import dj_database_url
 from dotenv import load_dotenv
 from pathlib import Path
@@ -81,18 +80,18 @@ WSGI_APPLICATION = 'Agent.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": dj_database_url.config(
-#         default=os.getenv("DATABASE_URL"), conn_max_age=1800
-#     ),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"), conn_max_age=1800
+    ),
+}
 
 
 
